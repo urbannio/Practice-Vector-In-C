666dd9676ed95e16fb92ee6a88d44b5893df9e63
urbannio <urbanniotsai@gmail.com>
2014-11-07 05:15:02 +0800

    Update VectorInC.h

--- VectorInC.h
+++ VectorInC.h
@@ -5,60 +5,64 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+
 enum _uVECTOR_TYPE
 {
-	uVECTOR_TYPE_INT		  = 1,
-	uVECTOR_TYPE_CHAR		  = 2,
-	uVECTOR_TYPE_FLOAT		  = 3,
-	uVECTOR_TYPE_DOUBLE	  	  = 4
+	uVECTOR_TYPE_INT		= 1,
+	uVECTOR_TYPE_CHAR		= 2,
+	uVECTOR_TYPE_FLOAT		= 3,
+	uVECTOR_TYPE_DOUBLE		= 4
 };
 typedef enum _uVECTOR_TYPE uVectorType;
 
 struct _uVector
 {
-/***********************
-*member variable       *
-***********************/
+    /***********************
+    *	member variable    *
+    ***********************/
 	void *container;
-	size_t elem_size;
 	
-	size_t max_num_elem;
-	size_t num_elem;
+	size_t elem_size;   // size of one element
+	size_t num_elem;    // number of element in the vector
+	size_t size_vec;    // (number of element in the vector) * (size of one element)
 	
-	size_t max_size_vec;
-	size_t size_vec;
+	uVectorType Type; 
 	
-	uVectorType Type;
-	
-/***********************
-*member function       *
-***********************/
+    /***********************
+    *	member function    *
+    ***********************/
 	
 	/*Capacity*/
-	size_t (*size)(struct _uVector*);	// point to _getSize, Returns the number of elements in the vector.
-	size_t (*max_size)(struct _uVector*); // point to _getMaxSize, Returns the maximum number of elements that the vector can hold.
-	void (*resize)(struct _uVector* ,size_t); // point to _resize, Resizes the container.
-	//size_t (*capacity)(struct _uVector*); // point to _capacity, Returns the size of the storage space currently allocated for the vector, expressed in terms of elements.
-	bool (*empty)(struct _uVector*); // point to _empty, test whether the vector is empty if it is empty returm true
-	void (*push_back)(struct _uVector*, void*); // point to _pushBack, Adds a new element at the end of the vector, after its current last element. The content of val is copied (or moved) to the new element.
-	void* (*at)(struct _vector*, size_t);
+	size_t   (*size)      (struct _uVector*);	                 // point to _getSize, Returns the number of elements in the vector.
+	void     (*resize)    (struct _uVector*, size_t);                // point to _resize, Resizes the container.
+	size_t   (*capacity)  (struct _uVector*);                        // point to _capacity, Returns the size of the storage space currently allocated for the vector, expressed in terms of elements.
+	bool     (*empty)     (struct _uVector*);                        // point to _empty, test whether the vector is empty if it is empty returm true
+	void     (*push_back) (struct _uVector*, void*);                 // point to _pushBack, Adds a new element at the end of the vector, after its current last element. The content of val is copied (or moved) to the new element.
+	void*    (*at)        (struct _uVector*, int);
+	void     (*pop_back)  (struct _uVector*);                        // point to _popBack, Removes the last element in the vector
+	
 	/*Modification*/
 
 };
-typedef struct _uVector uVector;
+typedef	struct _uVector uVector;
 
-
-uVector *newv(uVectorType ,size_t );
+/*******************************
+*     < non-member function >  *	
+*******************************/
+uVector *newv(uVectorType);
+void _debug(struct _uVector*);
 
 /***********************************************************
-*  < member function in struct _uVector >		   *
+*  < member function in struct _uVector >	           *
 *  note : start with '_' means it will                     *
 *  be pointed by another function has different name       *
 ***********************************************************/
-size_t _getSize(struct _uVector*);
-size_t _getMaxSize(struct _uVector*);
-void _resize(struct _uVector* ,size_t);
-bool _empty(struct _uVector*);
-void _pushBack(struct _uVector*, void*);
-void _at(struct _vector*, size_t);
+size_t  _size      (struct _uVector*);
+void    _resize    (struct _uVector*, size_t);
+void    _capacity  (struct _uVector*);
+bool    _empty     (struct _uVector*);
+void    _pushBack  (struct _uVector*, void*);
+void*   _at        (struct _uVector*, int);
+void    _popBack   (struct _uVector*);
+
 #endif
