13c96501cb456f341bcaed096c64cc13d93ac4e5
urbannio <urbannio@gmail.com>
2015-03-19 04:00:37 +0900

    fix space

--- VectorInC.c
+++ VectorInC.c
@@ -3,64 +3,64 @@
 uVector *newv(uVectorType uvectortype)
 {	
 	
-	uVector *uvector = (uVector*)malloc(sizeof(uVector));
+    uVector *uvector = (uVector*)malloc(sizeof(uVector));
 	
-	uvector->Type = uvectortype;
-	uvector->num_elem = 0;
-	uvector->size_vec = 0;  
+    uvector->Type = uvectortype;
+    uvector->num_elem = 0;
+    uvector->size_vec = 0;  
 
-	uvector->push_back  = &_pushBack;
-	uvector->at         = &_at;
-	uvector->pop_back   = &_popBack;
-	uvector->resize     = &_resize;
-	uvector->empty      = &_empty;
-	uvector->clear      = &_clear;
-	uvector->front      = &_front;
+    uvector->push_back  = &_pushBack;
+    uvector->at         = &_at;
+    uvector->pop_back   = &_popBack;
+    uvector->resize     = &_resize;
+    uvector->empty      = &_empty;
+    uvector->clear      = &_clear;
+    uvector->front      = &_front;
 	
-	switch(uvectortype)
-	{
-		case uVECTOR_TYPE_INT:
-			uvector->elem_size = sizeof(int);
-		break;
+    switch(uvectortype)
+    {
+        case uVECTOR_TYPE_INT:
+            uvector->elem_size = sizeof(int);
+	break;
 		
-		case uVECTOR_TYPE_CHAR:
-			uvector->elem_size = sizeof(char);
-		break;
+	case uVECTOR_TYPE_CHAR:
+	     uvector->elem_size = sizeof(char);
+	break;
 		
-		case uVECTOR_TYPE_FLOAT:
-			uvector->elem_size = sizeof(float);
-		break;
+	case uVECTOR_TYPE_FLOAT:
+	     uvector->elem_size = sizeof(float);
+	break;
 		
-		case uVECTOR_TYPE_DOUBLE:
-			uvector->elem_size = sizeof(double);
-		break;
-	}
+	case uVECTOR_TYPE_DOUBLE:
+	     uvector->elem_size = sizeof(double);
+	break;
+    }
 	
-	return uvector;
+    return uvector;
 }
 
 size_t _size(struct _uVector *uvector)
 {
-	return uvector->num_elem;
+    return uvector->num_elem;
 }
 
 void _pushBack(struct _uVector* uvector, void* newelem)
 {
-	uvector->resize(uvector,uvector->num_elem+1);
-	memcpy(uvector->container+uvector->num_elem*uvector->elem_size, newelem, uvector->elem_size);
-	uvector->num_elem++;
-	_debug(uvector);
+    uvector->resize(uvector,uvector->num_elem+1);
+    memcpy(uvector->container+uvector->num_elem*uvector->elem_size, newelem, uvector->elem_size);
+    uvector->num_elem++;
+    debug(uvector);
 }
 
 void* _at(struct _uVector* uvector, int offset)
 {
-	if(offset>uvector->num_elem)return NULL;
-	return uvector->container+offset*uvector->elem_size;
+    if(offset>uvector->num_elem)return NULL;
+    return uvector->container+offset*uvector->elem_size;
 }
 
 void* _front(struct _uVector* uvector)
 {
-	return uvector->container;
+    return uvector->container;
 }
 
 
@@ -68,66 +68,66 @@ void* _front(struct _uVector* uvector)
 void _resize(struct _uVector* uvector,size_t nsize)
 {	
 	
-	if(nsize==0)
-	{
-		uvector->clear(uvector);
-		return;
-	}
+    if(nsize==0)
+    {
+        uvector->clear(uvector);
+         return;
+    }
 	
-	if(uvector->size_vec==0)
-	{
-		uvector->container = malloc(nsize*uvector->Type);
-		uvector->size_vec = nsize*uvector->Type;
-		return;
-	}
-	if(nsize==uvector->num_elem)return;
+    if(uvector->size_vec==0)
+    {
+        uvector->container = malloc(nsize*uvector->Type);
+        uvector->size_vec = nsize*uvector->Type;
+        return;
+    }
+    if(nsize==uvector->num_elem)return;
 	
-	void *tmp = malloc(nsize*uvector->elem_size);
-	if(nsize<uvector->num_elem)
-	{
-		memcpy(tmp, uvector->container, nsize*uvector->elem_size);
-		uvector->num_elem = nsize;
-	}
-	else
-	{
-		memcpy(tmp, uvector->container, uvector->elem_size*uvector->num_elem);
-	}
+    void *tmp = malloc(nsize*uvector->elem_size);
+    if(nsize<uvector->num_elem)
+    {
+        memcpy(tmp, uvector->container, nsize*uvector->elem_size);
+        uvector->num_elem = nsize;
+    }
+    else
+    {
+        memcpy(tmp, uvector->container, uvector->elem_size*uvector->num_elem);
+    }
 	
-	free(uvector->container);
-	uvector->container = malloc(nsize*uvector->elem_size);
-	memcpy(uvector->container, tmp, uvector->elem_size*nsize);
-	free(tmp);
-	uvector->size_vec=nsize*uvector->elem_size;
-	_debug(uvector);
+    free(uvector->container);
+    uvector->container = malloc(nsize*uvector->elem_size);
+    memcpy(uvector->container, tmp, uvector->elem_size*nsize);
+    free(tmp);
+    uvector->size_vec=nsize*uvector->elem_size;
+    _debug(uvector);
 }
 
 void _clear(struct _uVector* uvector)
 {
-	free(uvector->container);
-	uvector->num_elem = 0;
-	uvector->size_vec = 0;
+    free(uvector->container);
+    uvector->num_elem = 0;
+    uvector->size_vec = 0;
 }
 
 void _popBack(struct _uVector* uvector)
 {
-	_resize(uvector,uvector->num_elem-1);
-	uvector->num_elem--;
+    _resize(uvector,uvector->num_elem-1);
+    uvector->num_elem--;
 }
 
 int _empty(struct _uVector* uvector)
 {
-	_debug(uvector);
-	if(uvector->num_elem>0)
-		return 0;
-	else 
-		return 1;
+    _debug(uvector);
+    if(uvector->num_elem>0)
+        return 0;
+    else 
+        return 1;
 }
 
 
 
 extern void _debug(struct _uVector* uvector)
 {
-	printf("elem_size    = %d\n",uvector->elem_size);
-	printf("num_elem     = %d\n",uvector->num_elem);
-	printf("size_vec     = %d\n\n",uvector->size_vec);
+    printf("elem_size    = %d\n",uvector->elem_size);
+    printf("num_elem     = %d\n",uvector->num_elem);
+    printf("size_vec     = %d\n\n",uvector->size_vec);
 }
--- VectorInC.h
+++ VectorInC.h
@@ -8,10 +8,10 @@
 
 enum _uVECTOR_TYPE
 {
-	uVECTOR_TYPE_INT		= 1,
-	uVECTOR_TYPE_CHAR		= 2,
-	uVECTOR_TYPE_FLOAT		= 3,
-	uVECTOR_TYPE_DOUBLE		= 4
+    uVECTOR_TYPE_INT    = 1,
+    uVECTOR_TYPE_CHAR   = 2,
+    uVECTOR_TYPE_FLOAT  = 3,
+    uVECTOR_TYPE_DOUBLE = 4
 };
 typedef enum _uVECTOR_TYPE uVectorType;
 
@@ -20,30 +20,30 @@ struct _uVector
     /***********************
     *	member variable    *
     ***********************/
-	void *container;
+    void *container;
 	
-	size_t elem_size;   // size of one element
-	size_t num_elem;    // number of element in the vector
-	size_t size_vec;    // (number of element in the vector) * (size of one element)
+    size_t elem_size;   // size of one element
+    size_t num_elem;    // number of element in the vector
+    size_t size_vec;    // (number of element in the vector) * (size of one element)
 	
 	
-	uVectorType Type; 
+    uVectorType Type; 
 	
     /***********************
     *	member function    *
     ***********************/
 	
-	/*Capacity*/
-	size_t   (*size)      (struct _uVector*);	                 // point to _getSize, Returns the number of elements in the vector.
-	void     (*resize)    (struct _uVector*, size_t);                // point to _resize, Resizes the container.
-	size_t   (*capacity)  (struct _uVector*);                        // point to _capacity, Returns the size of the storage space currently allocated for the vector, expressed in terms of elements.
-	int      (*empty)     (struct _uVector*);                        // point to _empty, test whether the vector is empty if it is empty returm true
-	void     (*push_back) (struct _uVector*, void*);                 // point to _pushBack, Adds a new element at the end of the vector, after its current last element. The content of val is copied (or moved) to the new element.
-	void*    (*at)        (struct _uVector*, int);
-	void     (*pop_back)  (struct _uVector*);                        // point to _popBack, Removes the last element in the vector
-	void     (*clear)     (struct _uVector*);
-	void*    (*front)     (struct _uVector*);
-	/*Modification*/
+    /*Capacity*/
+    size_t   (*size)      (struct _uVector*);	                     // point to _getSize, Returns the number of elements in the vector.
+    void     (*resize)    (struct _uVector*, size_t);                // point to _resize, Resizes the container.
+    size_t   (*capacity)  (struct _uVector*);                        // point to _capacity, Returns the size of the storage space currently allocated for the vector, expressed in terms of elements.
+    int      (*empty)     (struct _uVector*);                        // point to _empty, test whether the vector is empty if it is empty returm true
+    void     (*push_back) (struct _uVector*, void*);                 // point to _pushBack, Adds a new element at the end of the vector, after its current last element. The content of val is copied (or moved) to the new element.
+    void*    (*at)        (struct _uVector*, int);
+    void     (*pop_back)  (struct _uVector*);                        // point to _popBack, Removes the last element in the vector
+    void     (*clear)     (struct _uVector*);
+    void*    (*front)     (struct _uVector*);
+    /*Modification*/
 
 };
 typedef	struct _uVector uVector;
